46530b138cd6e3c5c1d0b877892d4a148f7932ef
Marcelo Cohen <mflashbr@gmail.com>
2017-03-02 21:15:17 -0300

    Mudanças finais

--- shakes2.py
+++ shakes2.py
@@ -1,4 +1,6 @@
 import string, re
+import numpy as np
+import matplotlib.pyplot as plt
 
 arq = open("shakespeare2.txt")
 dic = {}
@@ -18,6 +20,19 @@ arq.close()
 
 s = [(k, dic[k]) for k in sorted(dic, key=dic.get, reverse=True)]
 
+print(s[:20])
 print("Palavras distintas:",len(s))
 for k,v in s[:20]:
     print(k,'->',v)
+
+nums = np.linspace(1,20,20) # 1, 2, 3..., 20
+plt.xlabel("Palavras")
+plt.ylabel("Frequência")
+#plt.xticks(np.arange(13), calendar.month_abbr[0:13])
+dados1,dados2 = zip(*s[:20])
+print(dados1)
+print(dados2)
+plt.bar(nums,dados2)
+plt.xticks(nums,dados1)
+plt.show()
+
